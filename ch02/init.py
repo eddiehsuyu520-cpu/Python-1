e743cc120402397bec26df6bb8d7d2dc9fec9ea4
igor <mizikikou@gmail.com>
2026-07-21 10:07:52 +0800

    first commit

---
+++ ch02/init.py
@@ -0,0 +1,4 @@
+print(12)#
+print(0b1100)
+print(0o14)
+print(0x0c)
---
+++ ch02/stirng_slicing.py
@@ -0,0 +1,15 @@
+flavor = "0123456789abcde"
+print(flavor[0:3])
+print(flavor[3:7])
+print(flavor[3:])
+print(flavor[:])
+print(flavor[:14])
+print(flavor[13:15])
+print(flavor[-7:-4])
+print(flavor[-7:0])
+print(flavor[-7:])
+print(flavor[-3:])
+
+print(flavor[-1]) #代表最後一個字元。
+print(flavor[::2]) #從頭到尾，每隔 2 個字取一次。
+print(flavor[::-1]) #步長為 -1 代表倒著走，能直接把字串顛倒過來。
