cf98530c4a3257fd811bd5dd29cacf681d4b48a2
igor <mizikikou@gmail.com>
2026-07-29 16:23:59 +0800

    0729教學

---
+++ ch03/test.py
@@ -0,0 +1,7 @@
+print(f"[DEBUG] variable: {variable}")
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ ch04/break.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+while (s:= input('請輸入帳號：')) == 'gotop':
+        print('帳號正確!')
+        break
+else:
+  print('帳號錯誤!')
